c6bdcc391163eefb776ff07fcf6bf92c14d017ec
nayabjahan <nayabzafir@gmail.com>
2022-12-15 21:43:53 +0530

    Added:Functionality To create Contacts

--- src/main/java/com/blz/addressbook/AddressbookMain.java
+++ src/main/java/com/blz/addressbook/AddressbookMain.java
@@ -3,5 +3,6 @@ package com.blz.addressbook;
 public class AddressbookMain {
     public static void main(String[] args) {
         System.out.println("welcome to addressbook program");
+        
     }
 }
