5575b4b79192acf15afeb911fbaa3d8f440e7464
nayabjahan <nayabzafir@gmail.com>
2022-12-15 21:02:52 +0530

    Added:AddressBook program

---
+++ README.md
@@ -0,0 +1,1 @@
+# Addressbookk_RFP
---
+++ src/main/java/com/blz/addressbook/AddressbookMain.java
@@ -0,0 +1,7 @@
+package com.blz.addressbook;
+
+public class AddressbookMain {
+    public static void main(String[] args) {
+        System.out.println("welcome to addressbook program");
+    }
+}
